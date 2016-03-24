0da550d64c565aa1819ae6e722d3a316e37a808f
Declan van Welie <declanvanwelie@Wendys-iMac.local>
2016-03-23 23:41:05 -0400

    Adding basic files and gulp architecture

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+"Use strict";
+
+var gulp = require('gulp');
+   gulpLoadPlugins = require('gulp-load-plugins'),
+   plugins = gulpLoadPlugins(),
+   app = {};
+var del = require('del');
+
+var config = {
+
+   sassFiles: './public/assets/sass',
+   compiledDest: './public/assets'
+ //   // The vendor/bower_components directory is where the bower-installed third-party code lives,
+ //   // like Twitter Bootstrap
+ //   bowerDir: './bower_components'
+};
+
+// // Gulp Bower Task
+gulp.task('bower', function() {
+    return plugins.bower('./bower_components')
+        .pipe(gulp.dest(config.compiledDest))
+})
+
+gulp.task('clean', function(){
+     // Delete the assets folder
+    del.sync(config.compiledDest + '/css/*');
+
+});
+
+// Dynamic addStyle class creates sourcemaps, converts less into css, concatenates, and minifies css
+app.addStyle = function(paths) {
+
+    return gulp.src(paths)
+
+    .pipe(plugins.sass(paths))
+
+    .pipe(gulp.dest(config.compiledDest + '/css/'))
+
+};
+
+gulp.task('styles', ['clean'], function(){
+
+    return app.addStyle([
+        config.sassFiles + '/**/*.scss'
+    ])
+
+});
+
+// Watch task that listens for changes in the files contained within the listed directories.
+gulp.task('watch', ['styles'], function () {
+
+    // The following makes it so that the js will only be uglified when we run the gulp --production tag
+    // plugins.util.noop() does absolutely nothing
+    config.production ? plugins.util.noop() : gulp.watch(config.sassFiles + '/**/*.scss', ['styles']);
+});
+
+gulp.task('default', ['clean', 'styles', 'watch']);
+
